3994794a8dd2719c1674ee667653a1728ede1882
Michel <nicolas.herbaut@univ-paris1.fr>
2018-11-24 21:33:10 +0100

    added more unit tests for coverage

--- student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
+++ student-repository/src/test/java/fr/pantheonsorbonne/miage/AppTest.java
@@ -24,8 +24,8 @@ public class AppTest {
 	public void shouldAnswerWithTrue() throws IOException {
 		File tempDB = Files.createTempFile("prefix", ".csv").toFile();
 		FileWriter fw = new FileWriter(tempDB);
-		fw.write("1,Nicolas,Dr.\n");
-		fw.write("1,Francois,M.\n");
+		fw.write("Nicolas,Dr.,1,nico\n");
+		fw.write("Francois,M.,2,franco\n");
 		fw.close();
 
 		assertEquals(2, Iterables.size(StudentRepository.withDB(tempDB.toString())));
@@ -35,12 +35,25 @@ public class AppTest {
 		assertEquals("Nicolas", nicolas.getName());
 		assertEquals("Dr.", nicolas.getTitle());
 		assertEquals(1, nicolas.getId());
+		assertEquals("nico", nicolas.getPassword());
 
-		Student francois = Iterables.get(StudentRepository.withDB(tempDB.toString()), 0);
+		Student francois = Iterables.get(StudentRepository.withDB(tempDB.toString()), 1);
 
 		assertEquals("Francois", francois.getName());
 		assertEquals("M.", francois.getTitle());
 		assertEquals(2, francois.getId());
+		assertEquals("franco", francois.getPassword());
+		
+		StudentRepository.withDB(tempDB.toString()).add(new Student(3, "Mohamed", "M.", "momo"));
+		
+		assertEquals(2, Iterables.size(StudentRepository.withDB(tempDB.toString())));
+		
+		Student mohamed = Iterables.get(StudentRepository.withDB(tempDB.toString()), 1);
+
+		assertEquals("Mohamed", mohamed.getName());
+		assertEquals("M.", mohamed.getTitle());
+		assertEquals(3, mohamed.getId());
+		assertEquals("momo", mohamed.getPassword());
 
 	}
 }
