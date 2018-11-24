f10d3402da6e61df4bb6d408d2ff155f9f13e1e2
Nicolas <nicolas.herbaut@univ-paris1.fr>
2018-11-24 19:04:51 +0100

    added encryption as a Decorator pattern

---
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/EncryptedDiplomaGeneratorDecorator.java
@@ -0,0 +1,53 @@
+package fr.pantheonsorbonne.miage;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import fr.pantheonsorbonne.miage.diploma.DiplomaSnippet;
+
+public class EncryptedDiplomaGeneratorDecorator extends DiplomaGeneratorDecorator {
+
+	private String password;
+
+	public EncryptedDiplomaGeneratorDecorator(DiplomaGenerator other, String password) {
+		super(other);
+		this.password = password;
+	}
+
+	@Override
+	public InputStream getContent() {
+
+		try (InputStream is = other.getContent()) {
+			try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+				PdfReader pdfReader = new PdfReader(is);
+				PdfStamper pdfStamper = new PdfStamper(pdfReader, os);
+
+				pdfStamper.setEncryption(null, this.password.getBytes(), 0,
+						PdfWriter.ENCRYPTION_AES_256 );
+				pdfStamper.close();
+				return new ByteArrayInputStream(os.toByteArray());
+			}
+
+		} catch (IOException | DocumentException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException("failed to generate Encrypted File");
+		}
+
+	}
+
+	@Override
+	protected Collection<DiplomaSnippet> getDiplomaSnippets() {
+		return Collections.emptyList();
+	}
+
+}
