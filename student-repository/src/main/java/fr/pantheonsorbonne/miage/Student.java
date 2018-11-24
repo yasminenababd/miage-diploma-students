727e16acf448c8fb815cb9f7fc251ec56747119e
Mohamed <nicolas.herbaut@univ-paris1.fr>
2018-11-24 18:37:51 +0100

    intial version of the student repo

---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/Student.java
@@ -0,0 +1,45 @@
+package fr.pantheonsorbonne.miage;
+
+public class Student {
+
+	private int id;
+
+
+	public Student(int id, String name, String title) {
+		this.name = name;
+		this.title = title;
+		this.id = id;
+
+	}
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	private String title;
+
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return this.getTitle() + " " + this.getName();
+	}
+
+	
+}
---
+++ student-repository/src/main/java/fr/pantheonsorbonne/miage/StudentRepository.java
@@ -0,0 +1,78 @@
+package fr.pantheonsorbonne.miage;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+
+public class StudentRepository implements Iterable<Student> {
+
+	private String db;
+	private java.util.Iterator<Student> currentIterator = null;
+
+	private StudentRepository(String db) {
+		this.db = db;
+	};
+
+	public static StudentRepository withDB(String db) {
+		return new StudentRepository(db);
+	}
+
+	public static List<String> toReccord(Student stu) {
+
+		return Arrays.asList(stu.getName(), stu.getTitle(), "" + stu.getId());
+	}
+
+	public StudentRepository add(Student s) {
+		Iterator<Student> previousContent = StudentRepository.withDB(this.db).iterator();
+		try (FileWriter writer = new FileWriter(this.db)) {
+			CSVPrinter csvFilePrinter = new CSVPrinter(writer, CSVFormat.DEFAULT);
+
+			previousContent.forEachRemaining(student -> {
+				try {
+					csvFilePrinter.printRecord(toReccord(student));
+				} catch (IOException e) {
+					throw new RuntimeException("failed to update db file");
+				}
+			});
+			csvFilePrinter.printRecord(toReccord(s));
+			csvFilePrinter.flush();
+			csvFilePrinter.close(true);
+
+		} catch (IOException e) {
+			throw new RuntimeException("failed to update db file");
+		}
+		return this;
+
+	}
+
+	@Override
+	public java.util.Iterator<Student> iterator() {
+		try (FileReader reader = new FileReader(this.db)) {
+			
+
+			CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT);
+			this.currentIterator = parser.getRecords().stream()
+					.map((reccord) -> new Student(Integer.parseInt(reccord.get(2)), reccord.get(0), reccord.get(1)))
+					.map(c -> (Student) c).iterator();
+			return this.currentIterator;
+
+		} catch (IOException e) {
+			Logger.getGlobal().info("IO PB" + e.getMessage());
+			return Collections.EMPTY_SET.iterator();
+		}
+	}
+
+}
