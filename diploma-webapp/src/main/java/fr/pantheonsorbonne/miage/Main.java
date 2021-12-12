4cc7ce96ae1bf662d4804337a6e7af8c980e1780
yasminenababd <92510300+yasminenababd@users.noreply.github.com>
2021-12-12 12:04:32 +0100

    Update Main.java

--- diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
+++ diploma-webapp/src/main/java/fr/pantheonsorbonne/miage/Main.java
@@ -65,9 +65,9 @@ public class Main {
 		ArrayList<Student> students = new ArrayList<>();
 		Iterables.addAll(students, repo);
 
-		for (int i = 0; i < students.size(); i++) {
-			if (i == studentId) {
-				return students.get(i);
+		for (Student student:students) {
+			if (student.getId() == studentId) {
+				return student;
 			}
 		}
 
